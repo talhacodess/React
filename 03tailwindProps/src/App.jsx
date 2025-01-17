5d0f7626a402263a3aa7a6c4fa526ff48aefb40c
Talha Bashir <contact@talhacodes.com>
2025-01-17 15:50:30 +0000

    commit

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='bg-slate-700 text-yellow-400 p-5 text-6xl'>Props Practice</h1>
+     <Card/>
+    </>
+  )
+}
+
+export default App
